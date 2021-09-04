8d81aa8e9de02ab1463901788fc967ddb69436da
fetuffani <felipe_tuffani@hotmail.com>
2021-09-04 18:45:53 -0300

    chore: regen site

---
+++ docs/_next/static/q_ZqtlbhiwVXQ4aam6YOw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/807-1d99abbb8e01b867d672.js","static/chunks/pages/index-75005ffde4c460654f28.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
